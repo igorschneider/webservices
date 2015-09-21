a608b570615374b68a63ed7735592194ce868d1b
igorschneider <ioschneider@gmail.com>
2015-09-21 02:05:03 -0500

    Hibernate configuration and ProductOwner mapping

--- src/edu/luc/lakezon/product/ProductOwner.java
+++ src/edu/luc/lakezon/product/ProductOwner.java
@@ -1,8 +1,33 @@
 package edu.luc.lakezon.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "productowner")
 public class ProductOwner {
 	
+	@Id
+	@SequenceGenerator(name = "seq-gen", sequenceName = "productownerid_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq-gen")
+	@Column(name = "productownerid")
+	private int productOwnerId;
+	
+	@Column(name = "name")
 	private String name;
+
+	public int getProductOwnerId() {
+		return productOwnerId;
+	}
+
+	public void setProductOwnerId(int productOwnerId) {
+		this.productOwnerId = productOwnerId;
+	}
 
 	public String getName() {
 		return name;
---
+++ src/test/java/ProductOwnerTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import edu.luc.lakezon.product.ProductOwner;
+
+public class ProductOwnerTest {
+
+	@Test
+	public void testSave() {
+		
+		ProductOwner productOwner = new ProductOwner();
+		productOwner.setName("Amazon");
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		session.save(productOwner);
+		
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+		
+		assertTrue(true);
+	}
+
+}
