1abd5d2e5dac9a4baa273f892e7d991ecff7f456
igorschneider <ioschneider@gmail.com>
2015-09-22 20:06:52 -0500

    Hibernate in Address, Customer and Review

--- src/edu/luc/lakezon/customer/Address.java
+++ src/edu/luc/lakezon/customer/Address.java
@@ -1,14 +1,47 @@
 package edu.luc.lakezon.customer;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
 public class Address {
 
+	@Id
+	@SequenceGenerator(name = "seq-gen", sequenceName = "addressid_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq-gen")
+	@Column(name = "addressid")
+	private Integer addressId;
+	
+	@Column(name = "addressline1")
 	private String addressline1;
+	
+	@Column(name = "addressline2")
 	private String addressline2;
+	
+	@Column(name = "zipcode")
 	private String zipcode;
+	
+	@Column(name = "city")
 	private String city;
+	
+	@Column(name = "state")
 	private String state;
+	
+	@Column(name = "country")
 	private String country;
 
+	public Integer getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
+	}
 	public String getAddressline1() {
 		return addressline1;
 	}
@@ -45,7 +78,5 @@ public class Address {
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	
-	
-	
+
 }
--- src/edu/luc/lakezon/customer/Customer.java
+++ src/edu/luc/lakezon/customer/Customer.java
@@ -1,16 +1,49 @@
 package edu.luc.lakezon.customer;
 import java.util.Calendar;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
 	
+	@Id
+	@SequenceGenerator(name = "seq-gen", sequenceName = "customerid_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq-gen")
+	@Column(name = "customerid")
+	private Integer customerId;
 	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "gender")
 	private String gender;
+	
+	@Column(name = "birthdate")
 	private Calendar birthdate;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "addressid")
 	private Address address;
+	
+	@Column(name = "password")
 	private String password;
 	
-	
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
 	public String getName() {
 		return name;
 	}
