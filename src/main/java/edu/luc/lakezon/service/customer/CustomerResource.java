d2c5ce0ed21a72444cc04f8c44c71d8a24f79eea
igorschneider <ioschneider@gmail.com>
2015-10-18 18:37:57 -0500

    Customer service

---
+++ src/main/java/edu/luc/lakezon/service/customer/CustomerResource.java
@@ -0,0 +1,8 @@
+package edu.luc.lakezon.service.customer;
+
+import javax.ws.rs.Path;
+
+@Path("/customer/")
+public class CustomerResource {
+
+}
---
+++ src/main/java/edu/luc/lakezon/service/customer/CustomerService.java
@@ -0,0 +1,8 @@
+package edu.luc.lakezon.service.customer;
+
+import javax.jws.WebService;
+
+@WebService
+public interface CustomerService {
+
+}
