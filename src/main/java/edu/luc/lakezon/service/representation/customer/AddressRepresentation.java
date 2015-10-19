25b9ea73383a16dd5b53ad9590e4e7e4a52d5ef1
Raphael Salvador <rsalvador1@luc.edu>
2015-10-18 19:54:24 -0500

    Address

---
+++ src/main/java/edu/luc/lakezon/service/representation/customer/AddressRepresentation.java
@@ -0,0 +1,72 @@
+package edu.luc.lakezon.service.representation.customer;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "Address")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+public class AddressRepresentation {
+	
+	private String addressId;
+	private String addressline1;
+	private String city;
+	private String country;
+	private String state;
+	private String zipcode;
+
+	public AddressRepresentation() {}
+
+	public String getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddressline1() {
+		return addressline1;
+	}
+
+	public void setAddressline1(String addressline1) {
+		this.addressline1 = addressline1;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
+
+}
